a1d59af4d0b30bef8ec870b19b64e78721593a93
hayjmuir <haymuir@gmail.com>
2020-02-14 02:07:21 -0700

    finalized JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,64 @@
-
-
-
-
-
-
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var guessesSoFar = [];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-// var computerGuess = computerChoiceText[fromCharCode(97+Math.floor(Math.random() * 26)];
+
 
 
 var directionsText = document.getElementById("directions-text");
-    var winsText = document.getElementById("wins-text");
-    var lossesText = document.getElementById("losses-text");
-    var guessesLeft = document.getElementById("guesses-left");
-    var guessesMade = document.getElementById("guesses-made");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guesses = document.getElementById("guesses-left");
+var guessesMade = document.getElementById("guesses-made");
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+document.onkeyup = function(event) {
 
 
 
-    document.onkeyup = function(event) {
+ var guess = event.key;
 
-
-        var computerGuess = "a";
-        var guess = event.key;
-        guessesSoFar.push(guess);
-        
-
+ if ((guess < "a") || (guess > "z"))
+  return;
+ guessesSoFar.push(guess);
 
 
 
-
-        // var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-
-
-
-
-
-
-       
-        
-        winsText.textContent = "wins: " + wins;
-        lossesText.textContent = "losses: " + losses;
-        guessesLeft.textContent = "Guesses left : " + ties;
-        guessesMade.textContent = "Your guesses so far : " + guessesSoFar;
+ if (guess != computerGuess) {
+  guessesLeft--
+ };
+ if (guess === computerGuess) {
+  wins++;
+  guessesLeft = 10;
+  computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  guessesSoFar = []
+ } else if (guessesLeft == 0) {
+  losses++
+ };
 
 
+ if (guessesLeft == 0) {
+  guessesLeft = 10;
+  computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  guessesSoFar = []
+ };
 
 
-    }
+
+
+
+
+ winsText.textContent = "wins: " + wins;
+ lossesText.textContent = "losses: " + losses;
+ guesses.textContent = "Guesses left : " + guessesLeft;
+ guessesMade.textContent = "Your guesses so far : " + guessesSoFar;
+
+
+
+
+}
